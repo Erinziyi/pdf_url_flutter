6a9329a28ea68785a2b4fb97fdcb6751010c3205
unknown <erin@peoplelogy.com>
2019-07-03 12:00:09 +0800

    fix Horizontal swipe

--- lib/main.dart
+++ lib/main.dart
@@ -3,7 +3,7 @@ import 'dart:io';
 
 import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_full_pdf_viewer/full_pdf_viewer_scaffold.dart';
+import 'package:flutter_pdfview/flutter_pdfview.dart';
 import 'package:path_provider/path_provider.dart';
 
 
@@ -65,22 +65,105 @@ class _MyAppState extends State<MyApp> {
   }
 }
 
-class PDFScreen extends StatelessWidget {
+
+class PDFScreen extends StatefulWidget {
   String pathPDF = "";
   PDFScreen(this.pathPDF);
 
   @override
+  _PdfViewPageState createState() => _PdfViewPageState();
+}
+
+class _PdfViewPageState extends State<PDFScreen> {
+  int _totalPages = 0;
+  int _currentPage = 0;
+  bool pdfReady = false;
+  PDFViewController _pdfViewController;
+
+  @override
   Widget build(BuildContext context) {
-    return PDFViewerScaffold(
-        appBar: AppBar(
-          title: Text("Document"),
-          actions: <Widget>[
-            IconButton(
-              icon: Icon(Icons.share),
-              onPressed: () {},
-            ),
-          ],
-        ),
-        path: pathPDF);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("My Document"),
+      ),
+      body: Stack(
+        children: <Widget>[
+          PDFView(
+            filePath: widget.pathPDF ,
+            autoSpacing: true,
+            enableSwipe: true,
+            pageSnap: true,
+            swipeHorizontal: true,
+            nightMode: false,
+            onError: (e) {
+              print(e);
+            },
+            onRender: (_pages) {
+              setState(() {
+                _totalPages = _pages;
+                pdfReady = true;
+              });
+            },
+            onViewCreated: (PDFViewController vc) {
+              _pdfViewController = vc;
+            },
+            onPageChanged: (int page, int total) {
+              setState(() {});
+            },
+            onPageError: (page, e) {},
+          ),
+          !pdfReady
+              ? Center(
+            child: CircularProgressIndicator(),
+          )
+              : Offstage()
+        ],
+      ),
+      floatingActionButton: Row(
+        mainAxisAlignment: MainAxisAlignment.end,
+        children: <Widget>[
+          _currentPage > 0
+              ? FloatingActionButton.extended(
+            backgroundColor: Colors.red,
+            label: Text("Go to ${_currentPage - 1}"),
+            onPressed: () {
+              _currentPage -= 1;
+              _pdfViewController.setPage(_currentPage);
+            },
+          )
+              : Offstage(),
+          _currentPage+1 < _totalPages
+              ? FloatingActionButton.extended(
+            backgroundColor: Colors.green,
+            label: Text("Go to ${_currentPage + 1}"),
+            onPressed: () {
+              _currentPage += 1;
+              _pdfViewController.setPage(_currentPage);
+            },
+          )
+              : Offstage(),
+        ],
+      ),
+
+
+    );
   }
 }
+
+
+
+
+//class PDFScreen extends StatelessWidget {
+//  String pathPDF = "";
+//  PDFScreen(this.pathPDF);
+//
+//  @override
+//  Widget build(BuildContext context) {
+//    return PDFViewerScaffold(
+//        appBar: AppBar(
+//          title: Text("Document"),
+//
+//        ),
+//        path: pathPDF);
+//  }
+//}
